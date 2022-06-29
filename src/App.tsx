e27933e4aa05a76fd5e25b918ae99e14ae56a1a9
Antonio Lins <antoniolins@uol.com.br>
2022-06-29 17:29:57 -0300

    Criação da  Sidebar...

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,31 @@ import { Post } from './Post';
 
 import './styles.css';
 
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+
 export function App(): JSX.Element {
 
   return (
     <div>
       <Header />
-    <Post 
-        author ="Antonio Lins"
-        content = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quae corporis commodi inventore cum temporibus. Totam vero quis cumque, earum, placeat numquam consequuntur, quasi tempore sit accusamus harum labore commodi."
-      />   
-      <Post 
-        author ="Diego Fernandes"
-        content = "Este é um post muito Legal "
-      /> 
+      <div className={styles.wrapper}>
+        <Sidebar/>
+            <main>
+
+              <Post 
+                  author ="Antonio Lins"
+                  content = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quae corporis commodi inventore cum temporibus. Totam vero quis cumque, earum, placeat numquam consequuntur, quasi tempore sit accusamus harum labore commodi."
+              />   
+              <Post 
+                  author ="Diego Fernandes"
+                  content = "Este é um post muito Legal "
+                
+              /> 
+
+            </main>
+        </div>
+
     </div>
   )
 
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} 
+                src='https://images.unsplash.com/photo-1656299882838-de57aede97e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50'
+
+            />
+
+        <div className={styles.profile}>
+            <strong>Antonio Lins</strong>
+            <span>Web Developer</span>
+        </div>
+
+        <footer>
+            <a href="#">
+                Editar seu Perfil
+            </a>
+        </footer>
+        </aside>
+    );
+}
